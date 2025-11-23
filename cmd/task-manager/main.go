1c5a44a71cc124cfd80b3731f369e0905b0c5455
Alyson Ryan Ullirsch <alysonryanullirsch@MacBook-Air-de-Alyson.local>
2025-11-23 13:20:49 -0300

    adicionando o pacote urfave

--- cmd/task-manager/main.go
+++ cmd/task-manager/main.go
@@ -1,5 +1,14 @@
-package task_manager
+package main
+
+import (
+	app "app/internal/usecase"
+	"log"
+	"os"
+)
 
 func main() {
-
+	aplicacao := app.Gerar()
+	if err := aplicacao.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ internal/usecase/app.go
@@ -0,0 +1,45 @@
+package usecase
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+func Gerar() *cli.App {
+	app := cli.NewApp()
+
+	app.Name = "Aplicação de linha de comando"
+	app.Usage = "Busca IPs e nomes de servidores na internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
